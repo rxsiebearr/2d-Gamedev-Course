42d800222a8bd3e736c7dceb39bc4b6e7abc4d68
rxsiebearr <chzou1@s.sfusd.edu>
2024-03-26 14:27:39 -0700

    L5

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -4,6 +4,7 @@ var max_speed := 600.0
 var velocity := Vector2(0,0)
 var boost_speed := 1500.0
 var normal_speed := 600.0
+var steering_factor:= 10.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
@@ -16,8 +17,10 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
-		
-	velocity = direction * max_speed
+	
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
--- 04.to_space_and_beyond_workbook/practices/L5.P1.steering/steering.gd
+++ 04.to_space_and_beyond_workbook/practices/L5.P1.steering/steering.gd
@@ -15,6 +15,9 @@ func _process(delta: float) -> void:
 
 	# Add code to calculate the desired velocity, the steering,
 	# and add to the current velocity before changing the position.
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 
 	if direction.length() > 0.0:
