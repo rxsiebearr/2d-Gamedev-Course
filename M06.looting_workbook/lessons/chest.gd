fbac00351fe0dd19b09f8ab29d4d4d0802eea606
rxsiebearr <chzou1@s.sfusd.edu>
2024-04-29 14:42:18 -0700

    L5

--- M06.looting_workbook/lessons/chest.gd
+++ M06.looting_workbook/lessons/chest.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
+	
 func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 	mouse_entered.connect(_on_mouse_entered)
@@ -17,3 +22,12 @@ func _on_mouse_exited() -> void:
 
 func set_outline_thickness(new_thickness:float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _input_event(viewport: Node, event: InputEvent, shape_idex: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)
+	if event_is_mouse_click:
+		open()
