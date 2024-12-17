a8569ef75a12effd6b7896e2209cd6ce642f45c1
rxsiebearr <chzou1@s.sfusd.edu>
2024-12-17 10:32:33 -0800

    L15

--- M09.top_down_movement_workbook/lessons/ray_cast_2d.gd
+++ M09.top_down_movement_workbook/lessons/ray_cast_2d.gd
@@ -5,3 +5,5 @@ extends RayCast2D
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta: float) -> void:
 	global_position = get_global_mouse_position()
+	if is_colliding():
+		print(get_collision_point())
